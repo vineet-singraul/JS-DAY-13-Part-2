3348c928beafe7988afc5f6c03e3c973ebccc17c
Vineet Singraul <Singraulvineet20@gmail.com>
2024-12-24 17:35:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var buttone = document.querySelector("#btn");
+// buttone.addEventListener("click" , ()=>{
+//     alert("Hello Bro")
+// })
+
+// buttone.addEventListener("keydown" , ()=>{
+//     alert("Hello Bro")
+// })
+
+
+var inpu = document.querySelector("#inp");
+inpu.addEventListener("keydown" , (e)=>{
+    alert(e.key)
+})
